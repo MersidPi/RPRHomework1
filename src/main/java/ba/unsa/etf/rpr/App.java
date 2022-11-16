fe8f642d1fbcb0cb21eb08b70ba9438b086c8182
Mersid <mpilipovic1@etf.unsa.ba>
2022-11-16 18:11:02 +0100

    App class done

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -2,6 +2,8 @@ package ba.unsa.etf.rpr;
 
 /**
  * Entry point to ExpressionEvaluator class that implements Dijkstra's Algorithm for expression evaluation
+ * This is a console application, it accepts one string argument in which should be expression
+ * that needs to be evaluated, if it's valid, it returns correct result
  * @Author Mersid Pilipović
  */
 public class App {
@@ -11,10 +13,9 @@ public class App {
                 "\n\"( 6 * 3 )\",  \"( ( 6 * sqrt ( 8 ) ) - ( 5 / 6 ) )\",  \"sqrt ( 3 )\"" +
                 "\n\"( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )\" "+
                 "\nIf you don't follow these forms of expression, result may not be correct.");
-        String unos = " sqrt ( 3  ) ";
         try {
             System.out.println("\nEvaluating expression with Dijkstra's Algorithm..." +
-                    "\nResult: " + unos + " = " + ExpressionEvaluator.evaluate(unos));
+                    "\nResult: " + args[0] + " = " + ExpressionEvaluator.evaluate(args[0]));
         }
         catch (Exception e) {
             System.out.println("Exception was thrown: " + e);
